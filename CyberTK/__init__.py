491c27dfaa3fbb295534aa201c09f2f6408c2d0c
cetuch <53572644+cetuch@users.noreply.github.com>
2023-02-12 16:34:59 +0700

    Add files via upload

---
+++ CyberTK/__init__.py
@@ -0,0 +1,10 @@
+from .kullanici import CyberTKAPI
+
+
+
+__copyright__       = 'Copyright 2021-2022 CyberTK'
+__version__         = '1.0 - beta'
+__license__         = 'BSD 3-Clause'
+__author__          = 'CybeTK'
+__url__             = 'http://github.com/CyberTKR'
+__all__             = ["CyberTKAPI"]
